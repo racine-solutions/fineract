fba8d7bc1c666b632e5f11ef78079f7fef851680
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2025-09-21 12:06:13 +0300

    RPAY-64 Add more statistics

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/dataqueries/service/AnalyticalReportingServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/dataqueries/service/AnalyticalReportingServiceImpl.java
@@ -41,7 +41,7 @@ public class AnalyticalReportingServiceImpl implements AnalyticalReportingServic
                 "loan_stats.rejectedLoansCount, loan_stats.rejectedLoansAmount, " +
                 "loan_stats.totalInterestExpected, loan_stats.totalInterestPaid, " +
                 "loan_stats.totalPenaltiesGenerated, loan_stats.totalPenaltiesPaid, loan_stats.totalPenaltiesWaived, " +
-                "client_stats.totalClients, client_stats.activeClients,client_stats.pendingClients, " +
+                "client_stats.totalClients, client_stats.activeClients, client_stats.pendingClients, " +
                 "savings_stats.activeSavingsAccountsCount, savings_stats.activeSavingsAccountsAmount, " +
                 "savings_stats.pendingApprovalSavingsAccountsCount, savings_stats.pendingApprovalSavingsAccountsAmount, " +
                 "savings_stats.closedSavingsAccountsCount, savings_stats.closedSavingsAccountsAmount, " +
@@ -49,7 +49,13 @@ public class AnalyticalReportingServiceImpl implements AnalyticalReportingServic
                 "arrears_aging_stats.totalOverduePrincipal, arrears_aging_stats.totalOverdueInterest, " +
                 "arrears_aging_stats.totalOverdueFees, arrears_aging_stats.totalOverduePenalties, " +
                 "arrears_aging_stats.averageOverdueDays, arrears_aging_stats.maxOverdueDays, " +
-                "arrears_aging_stats.highRiskCount, arrears_aging_stats.mediumRiskCount, arrears_aging_stats.lowRiskCount " +
+                "arrears_aging_stats.highRiskCount, arrears_aging_stats.mediumRiskCount, arrears_aging_stats.lowRiskCount, " +
+                "todays_stats.loansActivatedTodayCount, todays_stats.loansActivatedTodayAmount, " +
+                "todays_stats.savingsActivatedTodayCount, todays_stats.savingsActivatedTodayAmount, " +
+                "client_growth_stats.newClientsThisMonth, client_growth_stats.newClientsThisQuarter, " +
+                "client_growth_stats.newClientsLast7Days, client_growth_stats.newClientsLast30Days, client_growth_stats.newClientsLast90Days, " +
+                "client_growth_stats.clientGrowthRateLast30Days, " +
+                "client_activity_stats.clientsWithActiveLoan, client_activity_stats.clientsWithActiveSavingsAccount, client_activity_stats.clientsWithBothLoanAndSavings " +
                 "FROM (" +
                 "    SELECT " +
                 "        COUNT(CASE WHEN l.loan_status_id = 300 THEN 1 END) AS activeLoansCount, " +
@@ -94,49 +100,144 @@ public class AnalyticalReportingServiceImpl implements AnalyticalReportingServic
                 "        COUNT(CASE WHEN DATEDIFF(CURDATE(), laa.overdue_since_date_derived) > 90 THEN 1 END) AS highRiskCount, " +
                 "        COUNT(CASE WHEN DATEDIFF(CURDATE(), laa.overdue_since_date_derived) BETWEEN 31 AND 90 THEN 1 END) AS mediumRiskCount, " +
                 "        COUNT(CASE WHEN DATEDIFF(CURDATE(), laa.overdue_since_date_derived) BETWEEN 1 AND 30 THEN 1 END) AS lowRiskCount " +
-                "    FROM m_loan_arrears_aging laa) AS arrears_aging_stats";
+                "    FROM m_loan_arrears_aging laa) AS arrears_aging_stats " +
+                "CROSS JOIN " +
+                "    (SELECT " +
+                "        COUNT(CASE WHEN l.disbursedon_date = CURDATE() THEN 1 END) as loansActivatedTodayCount, " +
+                "        SUM(CASE WHEN l.disbursedon_date = CURDATE() THEN l.principal_amount END) as loansActivatedTodayAmount, " +
+                "        (SELECT COUNT(CASE WHEN s.activatedon_date = CURDATE() THEN 1 END) FROM m_savings_account s) as savingsActivatedTodayCount, " +
+                "        (SELECT SUM(CASE WHEN s.activatedon_date = CURDATE() THEN s.account_balance_derived END) FROM m_savings_account s) as savingsActivatedTodayAmount " +
+                "    FROM m_loan l) as todays_stats " +
+                "CROSS JOIN " +
+                "    (SELECT " +
+                "        COUNT(CASE WHEN c.submittedon_date >= DATE_FORMAT(CURDATE(), '%Y-%m-01') THEN 1 END) as newClientsThisMonth, " +
+                "        COUNT(CASE WHEN c.submittedon_date >= DATE_FORMAT(CURDATE(), '%Y-%q-01') THEN 1 END) as newClientsThisQuarter, " +
+                "        COUNT(CASE WHEN c.submittedon_date >= CURDATE() - INTERVAL 7 DAY THEN 1 END) as newClientsLast7Days, " +
+                "        COUNT(CASE WHEN c.submittedon_date >= CURDATE() - INTERVAL 30 DAY THEN 1 END) as newClientsLast30Days, " +
+                "        COUNT(CASE WHEN c.submittedon_date >= CURDATE() - INTERVAL 90 DAY THEN 1 END) as newClientsLast90Days, " +
+                "        (COUNT(CASE WHEN c.submittedon_date >= CURDATE() - INTERVAL 30 DAY THEN 1 END) - COUNT(CASE WHEN c.submittedon_date >= CURDATE() - INTERVAL 60 DAY AND c.submittedon_date < CURDATE() - INTERVAL 30 DAY THEN 1 END)) / NULLIF(COUNT(CASE WHEN c.submittedon_date >= CURDATE() - INTERVAL 60 DAY AND c.submittedon_date < CURDATE() - INTERVAL 30 DAY THEN 1 END), 0) * 100 as clientGrowthRateLast30Days " +
+                "    FROM m_client c) as client_growth_stats " +
+                "CROSS JOIN " +
+                "    (SELECT " +
+                "        COUNT(DISTINCT l.client_id) as clientsWithActiveLoan, " +
+                "        (SELECT COUNT(DISTINCT s.client_id) FROM m_savings_account s WHERE s.status_enum = 300) as clientsWithActiveSavingsAccount, " +
+                "        COUNT(DISTINCT CASE WHEN l.client_id IS NOT NULL AND s.client_id IS NOT NULL THEN l.client_id END) as clientsWithBothLoanAndSavings " +
+                "    FROM m_loan l " +
+                "    INNER JOIN m_savings_account s ON l.client_id = s.client_id " +
+                "    WHERE l.loan_status_id = 300 AND s.status_enum = 300) as client_activity_stats";
 
-        jdbcTemplate.query(sql, rs -> {
-            details.setActiveLoansCount(rs.getLong("activeLoansCount"));
-            details.setActiveLoansAmount(rs.getBigDecimal("activeLoansAmount"));
-            details.setPendingApprovalLoansCount(rs.getLong("pendingApprovalLoansCount"));
-            details.setPendingApprovalLoansAmount(rs.getBigDecimal("pendingApprovalLoansAmount"));
-            details.setClosedLoansCount(rs.getLong("closedLoansCount"));
-            details.setClosedLoansAmount(rs.getBigDecimal("closedLoansAmount"));
-            details.setRejectedLoansCount(rs.getLong("rejectedLoansCount"));
-            details.setRejectedLoansAmount(rs.getBigDecimal("rejectedLoansAmount"));
-            details.setTotalInterestExpected(rs.getBigDecimal("totalInterestExpected"));
-            details.setTotalInterestPaid(rs.getBigDecimal("totalInterestPaid"));
-            details.setTotalPenaltiesGenerated(rs.getBigDecimal("totalPenaltiesGenerated"));
-            details.setTotalPenaltiesPaid(rs.getBigDecimal("totalPenaltiesPaid"));
-            details.setTotalPenaltiesWaived(rs.getBigDecimal("totalPenaltiesWaived"));
+        jdbcTemplate.query(sql, (rs) -> {
+            if (rs.next()) {
+                details.setActiveLoansCount(rs.getLong("activeLoansCount"));
+                details.setActiveLoansAmount(rs.getBigDecimal("activeLoansAmount"));
+                details.setPendingApprovalLoansCount(rs.getLong("pendingApprovalLoansCount"));
+                details.setPendingApprovalLoansAmount(rs.getBigDecimal("pendingApprovalLoansAmount"));
+                details.setClosedLoansCount(rs.getLong("closedLoansCount"));
+                details.setClosedLoansAmount(rs.getBigDecimal("closedLoansAmount"));
+                details.setRejectedLoansCount(rs.getLong("rejectedLoansCount"));
+                details.setRejectedLoansAmount(rs.getBigDecimal("rejectedLoansAmount"));
+                details.setTotalInterestExpected(rs.getBigDecimal("totalInterestExpected"));
+                details.setTotalInterestPaid(rs.getBigDecimal("totalInterestPaid"));
+                details.setTotalPenaltiesGenerated(rs.getBigDecimal("totalPenaltiesGenerated"));
+                details.setTotalPenaltiesPaid(rs.getBigDecimal("totalPenaltiesPaid"));
+                details.setTotalPenaltiesWaived(rs.getBigDecimal("totalPenaltiesWaived"));
 
-            details.setTotalClients(rs.getLong("totalClients"));
-            details.setActiveClients(rs.getLong("activeClients"));
-            details.setPendingClients(rs.getLong("pendingClients"));
+                details.setTotalClients(rs.getLong("totalClients"));
+                details.setActiveClients(rs.getLong("activeClients"));
+                details.setPendingClients(rs.getLong("pendingClients"));
 
-            details.setActiveSavingsAccountsCount(rs.getLong("activeSavingsAccountsCount"));
-            details.setActiveSavingsAccountsAmount(rs.getBigDecimal("activeSavingsAccountsAmount"));
-            details.setPendingApprovalSavingsAccountsCount(rs.getLong("pendingApprovalSavingsAccountsCount"));
-            details.setPendingApprovalSavingsAccountsAmount(rs.getBigDecimal("pendingApprovalSavingsAccountsAmount"));
-            details.setClosedSavingsAccountsCount(rs.getLong("closedSavingsAccountsCount"));
-            details.setClosedSavingsAccountsAmount(rs.getBigDecimal("closedSavingsAccountsAmount"));
-            details.setRejectedSavingsAccountsCount(rs.getLong("rejectedSavingsAccountsCount"));
-            details.setRejectedSavingsAccountsAmount(rs.getBigDecimal("rejectedSavingsAccountsAmount"));
+                details.setActiveSavingsAccountsCount(rs.getLong("activeSavingsAccountsCount"));
+                details.setActiveSavingsAccountsAmount(rs.getBigDecimal("activeSavingsAccountsAmount"));
+                details.setPendingApprovalSavingsAccountsCount(rs.getLong("pendingApprovalSavingsAccountsCount"));
+                details.setPendingApprovalSavingsAccountsAmount(rs.getBigDecimal("pendingApprovalSavingsAccountsAmount"));
+                details.setClosedSavingsAccountsCount(rs.getLong("closedSavingsAccountsCount"));
+                details.setClosedSavingsAccountsAmount(rs.getBigDecimal("closedSavingsAccountsAmount"));
+                details.setRejectedSavingsAccountsCount(rs.getLong("rejectedSavingsAccountsCount"));
+                details.setRejectedSavingsAccountsAmount(rs.getBigDecimal("rejectedSavingsAccountsAmount"));
 
-            details.setTotalOverduePrincipal(rs.getBigDecimal("totalOverduePrincipal"));
-            details.setTotalOverdueInterest(rs.getBigDecimal("totalOverdueInterest"));
-            details.setTotalOverdueFees(rs.getBigDecimal("totalOverdueFees"));
-            details.setTotalOverduePenalties(rs.getBigDecimal("totalOverduePenalties"));
-            details.setAverageOverdueDays(rs.getDouble("averageOverdueDays"));
-            details.setMaxOverdueDays(rs.getLong("maxOverdueDays"));
+                details.setTotalOverduePrincipal(rs.getBigDecimal("totalOverduePrincipal"));
+                details.setTotalOverdueInterest(rs.getBigDecimal("totalOverdueInterest"));
+                details.setTotalOverdueFees(rs.getBigDecimal("totalOverdueFees"));
+                details.setTotalOverduePenalties(rs.getBigDecimal("totalOverduePenalties"));
+                details.setAverageOverdueDays(rs.getDouble("averageOverdueDays"));
+                details.setMaxOverdueDays(rs.getLong("maxOverdueDays"));
 
-            final Map<String, Long> overdueLoansByRiskLevel = new java.util.HashMap<>();
-            overdueLoansByRiskLevel.put("High", rs.getLong("highRiskCount"));
-            overdueLoansByRiskLevel.put("Medium", rs.getLong("mediumRiskCount"));
-            overdueLoansByRiskLevel.put("Low", rs.getLong("lowRiskCount"));
-            details.setOverdueLoansByRiskLevel(overdueLoansByRiskLevel);
+                final Map<String, Long> overdueLoansByRiskLevel = new java.util.HashMap<>();
+                overdueLoansByRiskLevel.put("High", rs.getLong("highRiskCount"));
+                overdueLoansByRiskLevel.put("Medium", rs.getLong("mediumRiskCount"));
+                overdueLoansByRiskLevel.put("Low", rs.getLong("lowRiskCount"));
+                details.setOverdueLoansByRiskLevel(overdueLoansByRiskLevel);
+
+                details.setNewClientsThisMonth(rs.getLong("newClientsThisMonth"));
+                details.setNewClientsThisQuarter(rs.getLong("newClientsThisQuarter"));
+                details.setNewClientsLast7Days(rs.getLong("newClientsLast7Days"));
+                details.setNewClientsLast30Days(rs.getLong("newClientsLast30Days"));
+                details.setNewClientsLast90Days(rs.getLong("newClientsLast90Days"));
+                details.setClientGrowthRateLast30Days(rs.getBigDecimal("clientGrowthRateLast30Days"));
+
+                details.setClientsWithActiveLoan(rs.getLong("clientsWithActiveLoan"));
+                details.setClientsWithActiveSavingsAccount(rs.getLong("clientsWithActiveSavingsAccount"));
+                details.setClientsWithBothLoanAndSavings(rs.getLong("clientsWithBothLoanAndSavings"));
+
+                details.setLoansActivatedTodayCount(rs.getLong("loansActivatedTodayCount"));
+                details.setLoansActivatedTodayAmount(rs.getBigDecimal("loansActivatedTodayAmount"));
+                details.setSavingsActivatedTodayCount(rs.getLong("savingsActivatedTodayCount"));
+                details.setSavingsActivatedTodayAmount(rs.getBigDecimal("savingsActivatedTodayAmount"));
+            }
+            return details;
         });
+
+        final String genderSql = "SELECT cv.code_value, COUNT(c.id) as count FROM m_client c JOIN m_code_value cv ON c.gender_cv_id = cv.id GROUP BY c.gender_cv_id";
+        final Map<String, Long> clientsByGender = new HashMap<>();
+        jdbcTemplate.query(genderSql, (rs) -> {
+            while (rs.next()) {
+                clientsByGender.put(rs.getString("code_value"), rs.getLong("count"));
+            }
+            return clientsByGender;
+        });
+        details.setClientsByGender(clientsByGender);
+
+        final String ageSql = "SELECT " +
+                "SUM(CASE WHEN DATEDIFF(CURDATE(), c.date_of_birth)/365 < 18 THEN 1 ELSE 0 END) as '<18', " +
+                "SUM(CASE WHEN DATEDIFF(CURDATE(), c.date_of_birth)/365 BETWEEN 18 AND 25 THEN 1 ELSE 0 END) as '18-25', " +
+                "SUM(CASE WHEN DATEDIFF(CURDATE(), c.date_of_birth)/365 BETWEEN 26 AND 35 THEN 1 ELSE 0 END) as '26-35', " +
+                "SUM(CASE WHEN DATEDIFF(CURDATE(), c.date_of_birth)/365 BETWEEN 36 AND 45 THEN 1 ELSE 0 END) as '36-45', " +
+                "SUM(CASE WHEN DATEDIFF(CURDATE(), c.date_of_birth)/365 BETWEEN 46 AND 55 THEN 1 ELSE 0 END) as '46-55', " +
+                "SUM(CASE WHEN DATEDIFF(CURDATE(), c.date_of_birth)/365 > 55 THEN 1 ELSE 0 END) as '>55' " +
+                "FROM m_client c";
+        final Map<String, Long> clientsByAgeGroup = new HashMap<>();
+        jdbcTemplate.query(ageSql, (rs) -> {
+            if (rs.next()) {
+                clientsByAgeGroup.put("<18", rs.getLong("<18"));
+                clientsByAgeGroup.put("18-25", rs.getLong("18-25"));
+                clientsByAgeGroup.put("26-35", rs.getLong("26-35"));
+                clientsByAgeGroup.put("36-45", rs.getLong("36-45"));
+                clientsByAgeGroup.put("46-55", rs.getLong("46-55"));
+                clientsByAgeGroup.put(">55", rs.getLong(">55"));
+            }
+            return clientsByAgeGroup;
+        });
+        details.setClientsByAgeGroup(clientsByAgeGroup);
+
+        final String loanProductSql = "SELECT p.name, COUNT(l.id) as count FROM m_loan l JOIN m_product_loan p ON l.product_id = p.id WHERE l.loan_status_id = 300 GROUP BY p.name";
+        final Map<String, Long> activeLoansByProduct = new HashMap<>();
+        jdbcTemplate.query(loanProductSql, (rs) -> {
+            while (rs.next()) {
+                activeLoansByProduct.put(rs.getString("name"), rs.getLong("count"));
+            }
+            return activeLoansByProduct;
+        });
+        details.setActiveLoansByProduct(activeLoansByProduct);
+
+        final String savingsProductSql = "SELECT p.name, COUNT(s.id) as count FROM m_savings_account s JOIN m_savings_product p ON s.product_id = p.id WHERE s.status_enum = 300 GROUP BY p.name";
+        final Map<String, Long> activeSavingsByProduct = new HashMap<>();
+        jdbcTemplate.query(savingsProductSql, (rs) -> {
+            while (rs.next()) {
+                activeSavingsByProduct.put(rs.getString("name"), rs.getLong("count"));
+            }
+            return activeSavingsByProduct;
+        });
+        details.setActiveSavingsByProduct(activeSavingsByProduct);
 
         return details;
     }
